c1be32a1b80c1fdc78d8ec44954c323d8a350128
vregina <vitoria_ranny@hotmail.com>
2024-01-09 15:37:56 -0300

    Add new splash screen

--- app/src/main/java/com/example/skinapp/MainActivity.kt
+++ app/src/main/java/com/example/skinapp/MainActivity.kt
@@ -10,11 +10,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.skinapp.ui.theme.SkinAppTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            Thread.sleep(1000)
+        }
         setContent {
             SkinAppTheme {
                 // A surface container using the 'background' color from the theme
